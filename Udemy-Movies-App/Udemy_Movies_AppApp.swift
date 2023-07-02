4c860ac006b68c3ed1920fd36d0990f9ff86b233
hhakangull <hakanngul@icloud.com>
2023-07-02 10:14:47 +0100

    Initial Commit

---
+++ Udemy-Movies-App/Udemy_Movies_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_Movies_AppApp.swift
+//  Udemy-Movies-App
+//
+//  Created by Hakan Gül on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_Movies_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
